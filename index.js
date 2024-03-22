af24b1dc7bed5995c0ead0663b795773d1092400
jiverson <jiverson222@gmail.com>
2024-03-22 12:28:20 -0700

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+import queryString from 'query-string';
+
+const location = {
+  search: '?foo=bar', hash: '#token=b'
+}
+
+console.log(location.search);
+//=> '?foo=bar'
+
+const parsed = queryString.parse(location.search);
+console.log(parsed);
+//=> {foo: 'bar'}
+
+console.log(location.hash);
+//=> '#token=bada55cafe'
+
+const parsedHash = queryString.parse(location.hash);
+console.log(parsedHash);
+//=> {token: 'bada55cafe'}
+
+parsed.foo = 'unicorn';
+parsed.ilike = 'pizza';
+
+const stringified = queryString.stringify(parsed);
+//=> 'foo=unicorn&ilike=pizza'
+
+location.search = stringified;
+// note that `location.search` automatically prepends a question mark
+console.log(location.search);
+//=> '?foo=unicorn&ilike=pizza'
